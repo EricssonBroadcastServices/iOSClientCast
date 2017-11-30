9342b548077e92869475e5d4c31e4bf7258e9d65
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-11-30 11:41:08 +0100

    Initial project structure

---
+++ Cast/Cast.h
@@ -0,0 +1,19 @@
+//
+//  Cast.h
+//  Cast
+//
+//  Created by Fredrik Sjöberg on 2017-11-30.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cast.
+FOUNDATION_EXPORT double CastVersionNumber;
+
+//! Project version string for Cast.
+FOUNDATION_EXPORT const unsigned char CastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cast/PublicHeader.h>
+
+
---
+++ UPGRADE_GUIDE.md
@@ -0,0 +1,4 @@
+# Upgrade Guide
+
+## Adopting 0.73.0
+Please consult the [Installation](https://github.com/EricssonBroadcastServices/iOSClientCast/blob/master/README.md#installation) and [Usage](https://github.com/EricssonBroadcastServices/iOSClientCast/blob/master/README.md#getting-started) guides for information about this initial release.
